c56c527aeba32edfc24c11192c5bf7b888806f51
IliaAlemasov <lastsunset666@gmail.com>
2023-02-21 18:57:36 +0700

    подумать над выбором технологии

--- 123.py
+++ 123.py
@@ -4,9 +4,19 @@ import imutils
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 image = cv2.imread('scr\\work_screenshot.jpeg')
-location, = hog.detectMultiScale(image)
-print((type(location)))
-print(location)
+location, vectors = hog.detectMultiScale(image, padding= (16,16), winStride= (2,2), scale= 1)
+for (x, y, w, h) in location:
+
+    cv2.rectangle(image, (x, y),
+
+                  (x + w, y + h),
+
+                  (0, 0, 255), 2)
+cv2.imshow("Image", image)
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
 
 
 
