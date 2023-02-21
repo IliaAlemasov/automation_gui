b7264ea84733c6d3ce65e8fd877c018b94e78cc2
IliaAlemasov <lastsunset666@gmail.com>
2023-02-21 11:22:36 +0700

    начинаю детектор людей

--- 123.py
+++ 123.py
@@ -1,1 +1,12 @@
+import cv2
+import imutils
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+image = cv2.imread('scr\\work_screenshot.jpeg')
+location, = hog.detectMultiScale(image)
+print((type(location)))
+print(location)
+
+
 
