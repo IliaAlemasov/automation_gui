bfcd53ea28f807444961939b4432e30f7e9cc705
IliaAlemasov <lastsunset666@gmail.com>
2023-09-14 14:19:45 +0700

    добавлена автоматизация одной надоедливой операции в премьер

---
+++ HOTKEYS_FOR_PREMIER.py
@@ -0,0 +1,17 @@
+import time
+import keyboard
+import pyautogui
+from def_for_PH_batch_T2 import *
+
+def del_and_shift():
+    button = check_button_on_screen('buttons\\del_and_shift.png')
+    delay_before_click()
+    click_on_center_button(button)
+    delay_before_click()
+    pyautogui.press('v')
+    delay_before_click()
+    pyautogui.moveTo(1953,1735 )
+
+keyboard.add_hotkey('1', del_and_shift)
+
+keyboard.wait()
