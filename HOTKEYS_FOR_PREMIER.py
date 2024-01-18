a7cacdb95944ada9147fa2bc43319864fc419dc4
IliaAlemasov <lastsunset666@gmail.com>
2024-01-18 08:24:08 +0700

    +1 починил увтоматизацию операции в премьер

--- HOTKEYS_FOR_PREMIER.py
+++ HOTKEYS_FOR_PREMIER.py
@@ -4,14 +4,14 @@ import pyautogui
 from def_for_PH_batch_T2 import *
 
 def del_and_shift():
-    button = check_button_on_screen('buttons\\del_and_shift.png',for_confidence=.7)
+    button = check_button_on_screen ('buttons\\del_and_shift.png',for_confidence=.7)
     delay_before_click()
     click_on_center_button(button)
-    delay_before_click()
-    pyautogui.press('v')
-    delay_before_click()
+    #delay_before_click()
+    #pyautogui.press('v')
+    #delay_before_click()
     pyautogui.moveTo(1953,1735)
 
-keyboard.add_hotkey('pause', del_and_shift)
+keyboard.add_hotkey('tab ', del_and_shift)
 
 keyboard.wait()
